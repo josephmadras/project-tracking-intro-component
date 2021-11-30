99040ad73c5a7ce5a6f932a23e76efdaf0ef04ad
Joseph Madras <josephkabemba77@gmail.com>
2021-11-30 04:30:32 +0100

    feat: add hamburger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburgerContainer = document.querySelector('.hamburger-container');
+const hamburger = document.querySelector('.hamburger');
+
+
+hamburgerContainer.addEventListener("click", () => {
+  // alert('Joseph Madras')
+  hamburger.classList.toggle("close-icon")
+})
